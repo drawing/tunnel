b2ffb45672b227b44bb618a8935734230b9c6a00
cppbreak <cppbreak@gmail.com>
2015-05-15 14:01:34 +0800

    add http parser

--- httptools/http_tunnel.go
+++ httptools/http_tunnel.go
@@ -1,4 +1,140 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+)
+
+func ReadSocks5(conn net.Conn) (ip, port string, err error) {
+	const (
+		IP_KIND     = 1
+		DOMAIN_KIND = 3
+	)
+
+	const max_socks5_header_len = 255
+	buffer := make([]byte, max_socks5_header_len)
+
+	// VER NMETHODS METHODS
+	_, err = io.ReadFull(conn, buffer[0:2])
+	if err != nil || buffer[0] != 0x5 {
+		return ip, port, errors.New("socks5 version error")
+	}
+
+	_, err = io.ReadFull(conn, buffer[0:buffer[1]])
+	if err != nil {
+		return ip, port, errors.New("socks5 methods error")
+	}
+
+	// REPLY: VER METHOD
+	conn.Write([]byte{0x05, 0x00})
+
+	// VER CMD RSV ATYP
+	_, err = io.ReadFull(conn, buffer[0:4])
+	if err != nil || buffer[0] != 0x5 || buffer[1] != 0x1 {
+		return ip, port, errors.New("cmd error")
+	}
+
+	// only support tcp now
+	length := 4
+	kind := buffer[3]
+	switch kind {
+	case IP_KIND:
+	case DOMAIN_KIND:
+		_, err = io.ReadFull(conn, buffer[0:1])
+		if err != nil {
+			return ip, port, errors.New("domain len error")
+		}
+		length = int(buffer[0])
+	default:
+		return ip, port, errors.New("unknown address type")
+	}
+
+	_, err = io.ReadFull(conn, buffer[0:length+2])
+	if err != nil {
+		return ip, port, errors.New("read address failed")
+	}
+
+	var addr_port uint16 = 80
+	binary.Read(bytes.NewBuffer(buffer[length:length+2]), binary.BigEndian, &addr_port)
+	switch kind {
+	case IP_KIND:
+		ip = net.IPv4(buffer[0], buffer[1], buffer[2], buffer[3]).String()
+		port = strconv.Itoa(int(addr_port))
+	case DOMAIN_KIND:
+		port = strconv.Itoa(int(addr_port))
+		ip = string(buffer[0:length])
+	}
+
+	conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0xca, 0x0a, 0x0a, 0xca, 0x33, 0x33})
+
+	return ip, port, nil
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	ip, port, err := ReadSocks5(conn)
+	if err != nil {
+		log.Println("socks5 failed:", err)
+		return
+	}
+
+	log.Println(ip, port)
+
+	reader := bufio.NewReader(conn)
+	req, err := http.ReadRequest(reader)
+	if err != nil {
+		log.Println("http failed:", err)
+		return
+	}
+
+	log.Println("REQ:", req)
+
+	client := &http.Client{}
+
+	// construct request
+
+	// redirect header
+
+	// redirect body
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("request failed:", err)
+		return
+	}
+
+	// write back header
+
+	// write back body
+
+	err = resp.Write(conn)
+	if err != nil {
+		log.Println("wirte resp failed:", err)
+		return
+	}
+}
+
 func main() {
+	ln, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalln("listen failed:", err)
+	}
+
+	log.Println("(9000) Running...")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("accept failed:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
